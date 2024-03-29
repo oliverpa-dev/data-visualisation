308952c419c0f74023dfe81c2012b436ea822680
Oliver <oliver.paszkiewicz0@gmail.com>
2024-03-29 17:10:53 +0100

    Add: logic to upload the csv data

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,29 @@ import "./App.css";
 
 function App() {
   return <div className="App"></div>;
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.files) {
+      Papa.parse(event.target.files[0], {
+        header: true,
+        skipEmptyLines: true,
+        complete: function (results) {
+          const dataXArr: any = {};
+          let oneGear: any = [];
+
+          results.data.forEach((entry: any) => {
+            if (entry.gear > 3 && !dataXArr[entry[""]]) {
+              dataXArr[entry[""]] = entry.gear;
+            }
+          });
+
+          if (dataX) {
+            setDataX(dataXArr);
+            setIsFetched(!isFetched);
+          }
+        },
+      });
+    }
+  };
 }
 
 export default App;
